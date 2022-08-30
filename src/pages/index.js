f1738f765abd379dd5db01fb7468ce528911c59e
qamar360 <112413604+qamar360@users.noreply.github.com>
2022-08-30 21:41:44 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,14 +34,13 @@ const Index = () => (
     </section>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">Services</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service One</p>
+              <p className="font-semibold text-xl">Web Design & Development</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Our web developers are client-centric, creating website solutions that are client-centric and deliver tangible business results. They help brands in an ever-changing digital environment.
               </p>
             </Card>
           </div>
