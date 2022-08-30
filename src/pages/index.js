e1c09fbb7be8ab93fcfc7eb5ea15c6d64eed1c4d
qamar360 <112413604+qamar360@users.noreply.github.com>
2022-08-30 22:12:26 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,9 +65,6 @@ const Index = () => (
    
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
       <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
-      <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
-      </p>
       <p className="mt-8">
         <Button size="xl">Get Started Now</Button>
       </p>
