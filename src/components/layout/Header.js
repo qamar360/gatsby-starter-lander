fa86db2408a91e38b557cf0a6ececeffe168be55
qamar360 <112413604+qamar360@users.noreply.github.com>
2022-08-30 22:14:34 +0300

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -14,21 +14,8 @@ const Header = () => (
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#features">
-          Features
-        </AnchorLink>
-        <AnchorLink className="px-4" href="#services">
           Services
         </AnchorLink>
-        <AnchorLink className="px-4" href="#stats">
-          Stats
-        </AnchorLink>
-        <AnchorLink className="px-4" href="#testimonials">
-          Testimonials
-        </AnchorLink>
-      </div>
-      <div className="hidden md:block">
-        <Button className="text-sm">Start Free Trial</Button>
-      </div>
     </div>
   </header>
 );
